7a184bbe5e9979303bd60edee86ab0e29ed27086
diegoferpit <diego.ferpit@outlook.es>
2025-11-11 10:24:51 +0100

    Cambiar el precio del billete

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -45,6 +45,10 @@ public class MaquinaExpendedoraSimple {
         return totalDineroAcumulado;
     }
     
+    public void setPrecioBillete(int precioDelBillete) {
+        precioBillete = precioDelBillete;
+    }
+   
     
 
     /**
